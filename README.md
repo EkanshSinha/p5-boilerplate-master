d68bec4a8a22a323d10956c0bd067c3eb9462d0a
EkanshSinha <71957034+EkanshSinha@users.noreply.github.com>
2021-06-27 18:39:19 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# p5-boilerplate-master
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ stone.js
@@ -0,0 +1,12 @@
+class stone
+{
+   constructor(x,y,w,h)
+   {
+       varoptions={
+    isStatic:false
+    resititution:0,
+    friftion:1,
+    density:1.2
+       }
+    }
+}
